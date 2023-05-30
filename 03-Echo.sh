e284f685cf203d7c89179cb621bacd9edbfd208f
Girish <girishiva1989@gmail.com>
2023-05-30 20:37:39 +0530

    Added Echo notes

---
+++ 03-Echo.sh
@@ -0,0 +1,9 @@
+##Enable colours
+
+# Syntex - echo -e "\e[COLmMESSAGE\e[0m"
+# -e - which enables/makes \e(Enable  Color in echo command) to work
+# All the input should be there in quotes (Double quotes preffere)
+# \e - Enables a color
+# COLm - Color code - Red[31m], Green[32m] , Yellow[33m], Blue[34m], Magenta[53m], Cyan[36m]
+# \e[0m - this is to disable the color, once we enable the color, we have to disable, else
+# color fallows to next line asw ell
