ee56dba5f81a40af83c912f0a1ebf04fddad362c
Girish <girishiva1989@gmail.com>
2023-06-18 14:03:59 +0530

    added functions for title

--- 05-Functions.sh
+++ 05-Functions.sh
@@ -8,3 +8,13 @@ function_name() {
 
 # Call a function
 function_name
+
+# We can inputs the function and we can access them with special variables $1-$n, $*, $#
+
+function_name1(){
+  echo function one = $1
+  echo all functions = $*
+  echo number of functions = $#
+}
+
+function_name1 abc 123
