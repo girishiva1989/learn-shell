97eda59c873b41a162182e1a7867e26243ae4925
Girish <girishiva1989@gmail.com>
2023-07-20 18:41:32 +0530

    Updating Shell topic

--- 07_alltopic.sh
+++ 07_alltopic.sh
@@ -30,10 +30,35 @@ echo c - $c
 # Declare a variable in CLI c=10, try to echo $c, but try to run the script having echo $c, we will not able to access the value
 # If we want access the variable, which is declared in CLI, we should convert the variable from local to  environment variable
 # In the CLI, type export c, and then run the script with echo $c, then we can see value 10
+
 #4. Inputs
+# Refer Variables.sh
+# Script has it's own input, and function has it own input
+
 #5. Conditions
+#Refer if.sh
+# If uses expressions
+# string expressions
+# Number expressions
+# File expressions
+# Case command - it can do only string comparision
+
 #6. Loops
+# IF we want to execute same command again and again, then loops will be used
+#Expression based loop
+#Input based loop
+
 #7. Functions
+# Refer function.sh
+#IF we declare a name to a set of commands is called as function
+# Variables inside main program can be accessed and modified inside a function
+# Function has its own exit status
+
 #8. Exit Status
+# Refer exitstatus.sh
+
 #9. Quotes
+#if there is any special character in input to the command then we use quotes
+# Single quote - to print as it is we use single quotes (does not allow variables)
+# Double quote - to print along with variable substitution ( Allow variables)
 #10. SED Editor
---
+++ 08_loop.sh
@@ -0,0 +1,12 @@
+a=10
+
+while [ $a -gt 0 ]; do
+  echo a - $a - grater then 0
+  a=$(($a-1))
+  sleep1
+done
+
+for fruit in apple bananna orange ; do
+  eho fruit name  - $fruit
+  sleep1
+done
