874e3be1c08b8d2208f558d2e31d56067ab2eca8
girish <girishiva1989@gmail.com>
2024-07-14 22:15:31 +1000

    Add Java function

--- 08-redirectors.sh
+++ 08-redirectors.sh
@@ -1,8 +1,12 @@
 # < is used to load the file to a command
 # > is used to send the output to a file
 
-# 1) > /tmp/roboshop.log - > execution logs will not be displayed,
+# 1) > /tmp/out - > execution logs will be redirected to specified log file
+# and not be displayed on the terminal
 # but error details will be still displayed when there is a failure
-# 2) &> /tmp/roboshop.log - &> will store both execution and error the logs,
+# 2) 2> /tmp/err - 2> error messages will be redirected to specified log file
+# and not be displayed on the terminal
+# 3) &> /tmp/roboshop.log - &> will store both execution and error the logs,
 # but will have only latest logs (ex: only currently executed command logs)
-# 3) &>> /tmp/roboshop.log - &>> will get all the logs
+# 4) &>> /tmp/roboshop.log - &>> will get all the logs
+# 5) &>/dev/null - in case if we don't need any output and error messages to a file
